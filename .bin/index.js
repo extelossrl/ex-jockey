c2daa03d32018ab3377a76cc845249ae4c861762
Giovanni Cavallo <giovannicavallo95@gmail.com>
2019-04-17 10:09:43 +0200

    add .bin folder

---
+++ .bin/index.js
@@ -0,0 +1,7 @@
+const path = require("path")
+const { exec } = require("child_process")
+const open = require("open")
+
+open("http://localhost:3000")
+exec("npm run server", { cwd: path.resolve(__dirname, "../") })
+exec("npm run dev", { cwd: path.resolve(__dirname, "../") })
--- index.js
+++
@@ -1,6 +0,0 @@
-const { exec } = require("child_process")
-const open = require("open")
-
-open("http://localhost:3000")
-exec("npm run server")
-exec("npm run dev")
